936224a9b7542dbe92471a46356d508c6130c25c
sergiohdc <sergioh@uw.edu>
2014-10-16 15:54:50 -0700

    update with visual message

--- 501Lab3-1.py
+++ 501Lab3-1.py
@@ -1,13 +1,14 @@
 # 
 # This program changes the filenames in a given directory to lower case and appends 'file' to the beginning of each filename
 import os
+print ('Running the UNPOFORATOR!')
 # Designates input folder location
 in_path = 'c:/Users/sergioh/downloads/501Lab3/text_files/'
 # Designates output folder
 mypath = 'c:/Users/sergioh/downloads/501Lab3/text_files_fixed/'
 if not os.path.isdir(mypath):
     os.makedirs(mypath)   
-
+print ('Mischieve managed!')
 # for loop tha goes through the folder 
 for root, dirs, files in os.walk(in_path):
     for file_lower in files:
@@ -19,3 +20,5 @@ for root, dirs, files in os.walk(in_path):
             os.rename(in_path + file_lower, mypath + "file_" + file_lower +'.txt')
         else:
             os.rename(in_path + file_lower, mypath + "file_" + file_split[0] + '.txt')
+
+        
